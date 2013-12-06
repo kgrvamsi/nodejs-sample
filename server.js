2f7c46697f6b93cb126500ed76883f38d53b1624
kgrvamsi <kotipallivamsi_vamsi@yahoo.com>
2013-12-06 17:57:29 +0530

    Update server.js
    Spaces

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ my_http.createServer(function(request,response){
     sys.puts("I got kicked");  
     response.writeHeader(200, {"Content-Type": "text/html"});  
     response.write("<h1>The Server is Installed successfully with Node</h1>"+
-    "Server Type"+os.type()+
-    "Server Arch Type"+os.arch());  
+    "Server Type"+  os.type() +"\n"+
+    "Server Arch Type"+  os.arch());  
     response.end();  
 }).listen(80);  
 sys.puts("Server Running on 8080");
