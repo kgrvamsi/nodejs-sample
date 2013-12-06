4d8183d4db75ddc78b03f5d8ac4ad20803546839
kgrvamsi <kotipallivamsi_vamsi@yahoo.com>
2013-12-06 17:24:12 +0530

    Update server.js
    Added few more lines for the system config

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var sys = require("sys"),  
-my_http = require("http");  
+my_http = require("http"),
+os =require("os");
 my_http.createServer(function(request,response){  
     sys.puts("I got kicked");  
     response.writeHeader(200, {"Content-Type": "text/html"});  
-    response.write("<h1>The Server is Installed successfully with Node</h1>");  
+    response.write("<h1>The Server is Installed successfully with Node</h1>"+
+    "Server Type"+os.type()+
+    "Server Arch Type"+os.arch());  
     response.end();  
 }).listen(80);  
 sys.puts("Server Running on 8080");
