377ffb80d354f25a7842c287209f35c90f9deb07
kgrvamsi <kotipallivamsi_vamsi@yahoo.com>
2013-12-16 17:51:20 +0530

    Update server.js
    testing

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ my_http.createServer(function(request,response){
     response.writeHeader(200, {"Content-Type": "text/html"});  
     response.write("<h1>The Server is Installed successfully with Node</h1>"+
     "Server Type\n"+  os.type() +"\n"+
+    "Testing"+
     "Server Arch Type"+  os.arch());  
     response.end();  
 }).listen(4000);  
